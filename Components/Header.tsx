5b98a3bd3a3d584a4e4fa7ed8857a8bdc754de8c
hchalin <hchalin98@gmail.com>
2023-02-18 19:26:14 -0500

    tailwind initial setup done. started to build header component.  having weird errors with tailwind where className declarations wont apply unless define in the parent component first????

---
+++ Components/Header.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+
+
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header>
+      {/* Left side  */}
+      <div className="flex flex-row items-center">
+        {/* Social Icons */}
+        <SocialIcon
+          url="https://instagram.com/_____.hayden._____"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://linkedin.com/in/haydenchalin"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+      </div>
+
+      {/* Right side */}
+      <div>
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden ">Get in touch</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
