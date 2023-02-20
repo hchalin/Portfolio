a1fcfbf2a59ff74fce829b3574ee43b2534975da
hchalin <hchalin98@gmail.com>
2023-02-19 21:43:23 -0500

    had issues with img loading. set loading->lazy and made sure to import the image and NOT put the direct path in

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Headshot from '../imgs/Headshot.jpg'
 
-import { Cursor, Typewriter, useTypewriter  } from 'react-simple-typewriter'
-import BackgroundCircles from './BackgroundCircles'
-type Props = {}
+import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Head from "next/head";
+type Props = {};
 
 function Hero({}: Props) {
   const [text, count] = useTypewriter({
-    words: ["Hi, I'm Hayden ",'<Climber />', '<Coder />', '<Fullstack Dev />'],
+    words: ["Hi, I'm Hayden ", "<Climber />", "<Coder />", "<Fullstack Dev />"],
     loop: true,
     delaySpeed: 2000,
-  })
+  });
 
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
+      <Image src={Headshot} width='200' height='200' loading="lazy"/>
       <h1>
         <span>{text}</span>
-        <Cursor cursorColor='#474790'/>
+        <Cursor cursorColor="#474790" />
       </h1>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
