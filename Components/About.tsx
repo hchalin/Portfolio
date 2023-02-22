40a0cd9e741043d778798bbc6814e00b7ce8260e
hchalin <hchalin98@gmail.com>
2023-02-22 14:39:36 -0500

    about section done for mobile.  need to correct the photo size on larger screens

--- Components/About.tsx
+++ Components/About.tsx
@@ -7,24 +7,28 @@ type Props = {};
 
 function About({}: Props) {
   return (
-    <div className="flex flex-col md:flex-row relative text-center h-screen md:text-left max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:flex-row md:text-left max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
 
-        {/* Note: Do Not use simicolons in tailwind */}
+      {/* Note: Do Not use simicolons in tailwind */}
       <motion.div
-      initial={{
-        x: -200,
-        opacity: 0
-      }}
-      whileInView={{
-          x:0,
-         opacity: 1
-       }}
-      viewport={{once: true}} // it will only slide into view once
-      transition={{duration: 1.2}}
-      className=""
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        whileInView={{
+          x: 0,
+          opacity: 1,
+        }}
+        // viewport={{ once: true }} // it will only slide into view once
+        transition={{ duration: 1.2 }}
       >
         <Image
           src={Headshot}
@@ -33,7 +37,40 @@ function About({}: Props) {
           className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
         />
       </motion.div>
-    </div>
+      {/* no spaces btw md: and px  */}
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-simibold">
+          Here is a <span className="underline decoration-[#474790]/50">little</span> background
+        </h4>
+        <motion.p
+      initial={{
+
+        opacity: 0,
+      }}
+      whileInView={{
+
+        opacity: 1,
+      }}
+      transition={{ duration: 1.2 }}
+      className="text-base"
+    >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, quia
+          exercitationem! Quibusdam provident cumque magni. Officiis non nemo
+          assumenda praesentium architecto quis debitis, provident, dolorem
+          quidem voluptas quibusdam esse suscipit! Aperiam porro cumque officiis
+          ex quod ducimus facilis vitae eveniet maiores ad consequuntur, vel at
+          sint officia sequi quisquam saepe itaque voluptatibus animi hic! Aut
+          reprehenderit magni inventore voluptate iusto? Sed deserunt modi earum
+          eveniet architecto cum, blanditiis veniam. Tempora incidunt eius
+          aliquam minima aspernatur voluptatibus a porro sapiente consectetur
+          eveniet, non rerum id, alias assumenda eos numquam impedit
+          perspiciatis. Totam vel, est repellat iure ad cumque quisquam
+          provident ullam repellendus possimus quidem impedit ex dolores alias
+          corrupti modi excepturi beatae placeat tempore, architecto deleniti
+          perferendis, aperiam illo! Dolorum, assumenda.
+        </motion.p>
+      </div>
+    </motion.div>
   );
 }
 
