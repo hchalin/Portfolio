15688eed126b4797c0396a9999a8d44e630dfad8
Hayden Chalin <hchalin98@gmail.com>
2023-02-27 14:41:43 -0500

    fixed error with using uuid for map. For right now, im just gonnna use the github logo for the image for each project. this will link to the respected repo

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -1,34 +1,41 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import { v4 as uuidv4 } from "uuid";
+import { SocialIcon } from "react-social-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-type Props = {}
+type Props = {};
 
 function Projects({}: Props) {
-
   // fetch the projects from sanity
-  const projects = [1,2,3,4]
+  const projects = [1, 2, 3, 4];
+  const uniqueKey = uuidv4();
 
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 '>
+    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 ">
       {/* Title */}
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
         Projects
       </h3>
-        {/* This is the background line... playing around with tailwinds 'skew' */}
-      <div className='w-full absolute top-[30%] bg-[#474790]/10 left-0 h-[500px] skew-y-12 '></div>
+      {/* This is the background line... playing around with tailwinds 'skew' */}
+      <div className="w-full absolute top-[30%] bg-[#474790]/10 left-0 h-[500px] skew-y-12 "></div>
 
       {/*  */}
-      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
-      {projects.map((project) => (
-        <div >
-          <Image
-          src=''
-          />
-        </div>
-      ))}
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project) => (
+          <div key={uniqueKey}>
+             <SocialIcon
+          url="https://github.com/hchalin/react-weather-app"
+          fgColor="grey"
+          target="_blank" //opens in new tab
+          bgColor="transparent"
+        />
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
