58920e027d557ff0c340e5b5ca417112b48d89a8
Hayden <hchalin98@gmail.com>
2023-02-28 13:35:40 -0500

    project section layout done.

--- Components/ProjectCard.tsx
+++ Components/ProjectCard.tsx
@@ -7,13 +7,14 @@ function ProjectCard({ index }: Props) {
     <article>
       <div
         className="w-screen md:w-[700px] m-10 flex-shrink-0 snap-center
-      snap-mandatory flext flex-col space-y-5 h-[600px] bg-[#292929] p-20"
+      snap-mandatory flext flex-col space-y-5 h-[500px] xl:h-[500px] bg-[#292929] opacity-40 hover:opacity-100 transition-opacity duration-300  p-20 rounded-xl"
       >
         <SocialIcon
           url="https://github.com/hchalin/react-weather-app"
           fgColor="grey"
           target="_blank" //opens in new tab
           bgColor="transparent"
+          className="cursor-pointer "
         />
 
         <div>
--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { v4 as uuidv4 } from "uuid";
+import { motion } from "framer-motion";
 
 import ProjectCard from "./ProjectCard";
 
@@ -12,26 +13,35 @@ function Projects({}: Props) {
   const uniqueKey = uuidv4();
 
   return (
-    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 ">
+    <motion.div
+      initial={{
+        opacity: 0
+      }}
+      whileInView={{
+        opacity: 1
+      }}
+      transition={{
+        duration: 1.5
+      }}
+    className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 ">
       {/* Title */}
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
         Projects
       </h3>
       {/* This is the background line... playing around with tailwinds 'skew' */}
-      <div className="w-full absolute top-[30%] bg-[#474790]/10 left-0 h-[500px] skew-y-12 "></div>
+      <div className="w-full absolute top-[30%] bg-[#474790]/20 left-0 h-[500px] skew-y-12 "></div>
 
       {/* Card Container */}
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20" >
         {projects.map((project, i) => (
           //  project card
           <div key={uniqueKey}
-          className=""
           >
             <ProjectCard index={i}  />
           </div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
