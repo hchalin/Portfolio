6fcb3653bd76b8f5ddd81064f4b1bf7eb67d1867
Hayden <hchalin98@gmail.com>
2023-03-01 18:08:09 -0500

    added gmail funtionality.

--- Components/ContactMe.tsx
+++ Components/ContactMe.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -13,10 +13,17 @@ type Inputs = {
 
 //custom type hooks
 function ContactMe({}: Props) {
-  const [autoFill, setAutoFill] = useState('Hello, message me')
+  const [autoFill, setAutoFill] = useState("Hello, message me");
 
   const { register, handleSubmit } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(
+      "hchalin98@gmail.com"
+    )}&su=${encodeURIComponent(formData.subject)}&body=${encodeURIComponent(
+      `Hi, my name is ${formData.name}. ${formData.message}`
+    )}`;
+    window.open(gmailLink, '_blank')
+  };
 
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
@@ -47,14 +54,36 @@ function ContactMe({}: Props) {
         </div>
 
         {/* FORM */}
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
           <div className="md:space-x-2 ">
-            <input {...register('name')} placeholder="Name" className="contact-input" type="text" />
-            <input {...register('email')} placeholder="Email" type="email" className="contact-input" />
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contact-input"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              type="email"
+              className="contact-input"
+            />
           </div>
 
-          <input {...register('subject')} placeholder="Subject" className="contact-input" type="text" />
-          <textarea {...register('message')} placeholder="Message" className="contact-input" />
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contact-input"
+            type="text"
+          />
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contact-input"
+          />
           <button className="bg-[#474790] py-5 px-10 rounded-md text-black font-bold text-lg hover:bg-[#565690] transition-bg duration-700">
             Submit
           </button>
