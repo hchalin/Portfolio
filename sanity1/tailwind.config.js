4b6bcf7a1ffc26f00b845f4c4df8fd2535fe4956
Hayden <hchalin98@gmail.com>
2023-03-03 09:18:11 -0500

    added app route for tailwind for when we convert to next 13

--- sanity1/tailwind.config.js
+++ sanity1/tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   content: [
 './pages/**/*.{js,ts,jsx,tsx}',
 './components/**/*.{js,ts,jsx,tsx}',
-// './app/**/*.{js,ts,jsx,tsx}',
+// this is for the new app dir in next 13
+'./app/**/*.{js,ts,jsx,tsx}',
 ],
   theme: {
     extend: {},
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}"
+    "./Components/**/*.{js,ts,jsx,tsx}",
+    // this is for the new app dir in next 13
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
