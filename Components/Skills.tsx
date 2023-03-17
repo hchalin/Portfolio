afece164d82c0e5885a9148c357a617da6cb4c6c
Hayden <hchalin98@gmail.com>
2023-03-17 09:47:06 -0400

    skill directionLeft added

--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -18,9 +18,14 @@ function Skills({skills}: Props) {
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skill for current profieciency</h3>
 
       <div className="grid grid-cols-4 gap-5 ">
-        {skills.map((skill)=>(
+        {skills?.slice(0, skills.length / 2).map((skill)=>(
         // {console.log(skill)}
           <SkillComponent key={skill._id} skill={skill}/>
+
+        ))}
+        {skills?.slice(skills.length / 2).map((skill)=>(
+        // {console.log(skill)}
+          <SkillComponent key={skill._id} skill={skill} directionLeft/>
 
         ))}
 
